77cfde495b2d029d9cf33fb19ca34e08af8292bd
Jake Kim <imjakekim@gmail.com>
2022-06-09 08:32:30 -0700

    Finish hashes_2.rb

--- hashes_2.rb
+++ hashes_2.rb
@@ -8,9 +8,9 @@ states = {
 }
 
 # Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
-#
-#
-#
+states.each do |full, abbr|
+  puts "#{abbr} is the abbreviation for #{full}"
+end
 
 
 # ======================================
@@ -24,9 +24,9 @@ birthdays = {
 }
 
 # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
-#
-#
-#
+birthdays.each do |name, dob|
+  puts "#{name}'s birthday is on #{dob}"
+end
 
 
 # ======================================
@@ -40,6 +40,9 @@ login_statuses = {
 }
 
 # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
-#
-#
-#
+puts "Friends currently logged in:"
+login_statuses.each do |name, status|
+  if status
+    puts name
+  end
+end
