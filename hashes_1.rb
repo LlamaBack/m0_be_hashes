d92a076064d72a42446c021b70a9e3c7a61c33b7
Jake Kim <imjakekim@gmail.com>
2022-06-09 08:32:07 -0700

    Finish hashes_1.rb

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,29 +1,29 @@
 
-Activity:
+#Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
 foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
 
-# Write code that prints all of the 'keys' of the foods variable 
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
-# YOUR CODE HERE
+print foods.keys
 
-# Write code that prints all of the 'values' of the foods variable 
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods.values
 
-# Write code that prints the value of the second food of the foods variable 
+# Write code that prints the value of the second food of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods["grapes"]
 
-# Write code that adds a food to the foods hash. 
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+foods["bacon"] = 55
+p foods
 
 
 #-------------------
@@ -36,17 +36,24 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+email1 = {
+  sender: "iamsender@gmail.com",
+  reciever: "iamreciever@gmail.com",
+  subject: "Fire Tweet",
+  timestamp: "4:37 PM August 13, 2019",
+  message: "Dearest reciever, Hope this email finds you well. Can you like my fire tweet i just posted?"
+}
 
 # Write code that prints your email hash to the terminal.
+p email1
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+p email1.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+p email1.values
 
 
 #-------------------
@@ -74,7 +81,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -88,12 +95,37 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+emails = [
+  {
+    sender: "iamsender@gmail.com",
+    reciever: "iamreciever@gmail.com",
+    subject: "Fire Tweet",
+    timestamp: "4:37 PM August 13, 2019",
+    message: "Dearest reciever, Hope this email finds you well. Can you like my fire tweet i just posted?"
+  },
+  {
+    sender: "iamsender@gmail.com",
+    reciever: "iamreciever@gmail.com",
+    subject: "Popularity chasing?",
+    timestamp: "4:37 PM August 14, 2019",
+    message: "Do I come off as vain to you? I wonder if i need to stop sending these emails to ask for likes. Like is this unhealthy?"
+  },
+  {
+    sender: "iamsender@gmail.com",
+    reciever: "iamreciever@gmail.com",
+    subject: "YOO CHECK THIS OUT",
+    timestamp: "4:37 PM August 15, 2019",
+    message: "YOOOOOO check out my twitter. i just launched something and I need the engagement"
+  }
+]
+
+puts emails
